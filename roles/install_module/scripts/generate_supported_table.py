245ad5ece58843ffbbd306e1d6fb3df7ad0b4ce9
Jakub Wlodek <jwlodek@bnl.gov>
2025-06-25 13:55:01 -0400

    Cleanup python linting

--- roles/install_module/scripts/generate_supported_table.py
+++ roles/install_module/scripts/generate_supported_table.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 import os
+
 import yaml
 
-print("Generating supported module table")
-
-with open("README.md", "r") as fp:
-    lines = fp.readlines()
 
 def autogen_table():
 
     module_table = ""
-    for dir in os.listdir("vars"):
-        if os.path.isdir(f"vars/{dir}"):
-            for version_file in os.listdir(f"vars/{dir}"):
-                with open(f"vars/{dir}/{version_file}") as fp:
-                    print(f"Detected module {dir} version file:  {version_file}")
+    for module_dir in os.listdir("vars"):
+        if os.path.isdir(f"vars/{module_dir}"):
+            for version_file in os.listdir(f"vars/{module_dir}"):
+                with open(f"vars/{module_dir}/{version_file}") as fp:
+                    print(f"Found {module_dir} version file: {version_file}")
                     version = version_file.split(".")[0]
-                    module = yaml.safe_load(fp)[f"{dir}_{version}"]
-                    module_table += f"{dir} | {module['version']} | [{dir}_{version}]({module['url']}/tree/{version})\n"
+                    module = yaml.safe_load(fp)[f"{module_dir}_{version}"]
+                    module_table += (
+                        f"{module_dir} | {module['version']} |"
+                        f" [{module_dir}_{version}]({module['url']}/tree/{version})\n"
+                    )
 
     return f"""<!-- BEGIN_AUTOGEN -->
 
@@ -25,12 +25,22 @@ Module | Version | Url
 --- | --- | ---
 {module_table}"""
 
-with open("README.md", "w") as fp:
-    table = autogen_table()
 
-    for line in lines:
-        if line.startswith("<!-- BEGIN_AUTOGEN -->"):
-            fp.write(table)
-            break
-        else:
-           fp.write(line)
+def update_table_in_readme():
+    print("Generating supported module table")
+
+    with open("README.md", "r") as fp:
+        lines = fp.readlines()
+
+    with open("README.md", "w") as fp:
+        table = autogen_table()
+
+        for line in lines:
+            if line.startswith("<!-- BEGIN_AUTOGEN -->"):
+                fp.write(table)
+                break
+            fp.write(line)
+
+
+if __name__ == "__main__":
+    update_table_in_readme()
