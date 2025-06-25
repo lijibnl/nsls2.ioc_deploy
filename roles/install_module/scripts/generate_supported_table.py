5eec6722af355a57f9403572f1d2bcef2aa5c501
Jakub Wlodek <jwlodek@bnl.gov>
2025-06-25 13:41:10 -0400

    Resolve markdown linter errors

--- roles/install_module/scripts/generate_supported_table.py
+++ roles/install_module/scripts/generate_supported_table.py
@@ -12,19 +12,18 @@ def autogen_table():
     module_table = ""
     for dir in os.listdir("vars"):
         if os.path.isdir(f"vars/{dir}"):
-            for version in os.listdir(f"vars/{dir}"):
-                with open(f"vars/{dir}/{version}") as fp:
-                    print(f"Detected module {dir} version file:  {version}")
-                    module = yaml.safe_load(fp)[f"{dir}_{version.split('.')[0]}"]
-                    module_table += f"{dir} | {module['version']} | {module['url']}\n"
+            for version_file in os.listdir(f"vars/{dir}"):
+                with open(f"vars/{dir}/{version_file}") as fp:
+                    print(f"Detected module {dir} version file:  {version_file}")
+                    version = version_file.split(".")[0]
+                    module = yaml.safe_load(fp)[f"{dir}_{version}"]
+                    module_table += f"{dir} | {module['version']} | [{dir}_{version}]({module['url']}/tree/{version})\n"
 
-    return f"""
-<!-- BEGIN_AUTOGEN -->
+    return f"""<!-- BEGIN_AUTOGEN -->
 
 Module | Version | Url
 --- | --- | ---
-{module_table}
-"""
+{module_table}"""
 
 with open("README.md", "w") as fp:
     table = autogen_table()
@@ -32,5 +31,6 @@ with open("README.md", "w") as fp:
     for line in lines:
         if line.startswith("<!-- BEGIN_AUTOGEN -->"):
             fp.write(table)
+            break
         else:
            fp.write(line)
