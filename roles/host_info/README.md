502d554aa6c5d0d7cc4382e7d5f6c5fcac2bf593
Jakub Wlodek <jwlodek@bnl.gov>
2025-08-04 09:36:31 -0400

    Add pytest tests for device specific role var files, add tektronix afg3k role, add base_stream_device module

--- roles/host_info/README.md
+++ roles/host_info/README.md
@@ -37,7 +37,6 @@ Then, at the top of your role, simply include this one.
 Variable name | Required | Purpose
 --------------|-----------|----------
 `host_config` | Yes | Contains dictionary with all host configuration as read via `nsls2.general.github_vars_facts`.
-`ioc_name` | No | If specified, will lookup `host_config[ioc_name]` and place it in the `ioc` variable.
 
 ## Output variables
 
@@ -50,4 +49,3 @@ Variable name | Purpose
 `host_info_softioc_group` | Group that will be assigned to generated files.
 `host_info_hostname` | Hostname without the domain.
 `host_info_ioc_list` | List of all IOCs configured for host.
-`ioc` | Special variable containing dictionary of IOC configuration requested if `ioc_name` is defined.
---
+++ tests/test_deploy_ioc_vars.py
@@ -0,0 +1,36 @@
+import pytest
+from pathlib import Path
+import os
+
+
+OPTIONAL_KEYS: dict[str, type] = {
+    "deploy_ioc_post_deploy_step": str,
+    "deploy_ioc_template_root_path": str,
+    "deploy_ioc_executable": str,
+    "deploy_ioc_standard_st_cmd": bool,
+    "deploy_ioc_standard_post_init": bool,
+    "deploy_ioc_use_common": bool,
+    "deploy_ioc_load_as_substitutions": bool,
+    "deploy_ioc_manage_iocs_nextport": int,
+    "deploy_ioc_as_dir_name": str,
+    "deploy_ioc_device_specific_env": dict,
+    "deploy_ioc_load_as_substitutions": bool,
+    "deploy_ioc_required_module": str,
+}
+
+DEPLOY_IOC_VARS_FILES = [os.path.splitext(f)[0] for f in os.listdir("roles/deploy_ioc/vars") if f.endswith(".yml")]
+
+pytestmark = pytest.mark.parametrize("deploy_ioc_var_file", DEPLOY_IOC_VARS_FILES, indirect=True)
+
+def test_deploy_ioc_var_file_has_matching_role(deploy_ioc_var_file):
+    assert os.path.exists(os.path.join("roles", deploy_ioc_var_file.name))
+
+def test_deploy_ioc_var_files_valid(deploy_ioc_var_file):
+    for key in deploy_ioc_var_file.data:
+        assert key in OPTIONAL_KEYS
+        assert type(deploy_ioc_var_file.data[key]) is OPTIONAL_KEYS[key]
+
+def test_deploy_ioc_var_file_required_module_exists(deploy_ioc_var_file):
+    if "deploy_ioc_required_module" in deploy_ioc_var_file.data:
+        if deploy_ioc_var_file.data["deploy_ioc_required_module"]:
+            assert os.path.exists(os.path.join("roles/install_module/vars", f"{deploy_ioc_var_file.data["deploy_ioc_required_module"]}.yml"))
