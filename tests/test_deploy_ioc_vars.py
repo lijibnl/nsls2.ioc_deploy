3c5fb811b68b11386ef3a2a3c81fb48879991086
Jakub Wlodek <jwlodek@bnl.gov>
2025-08-04 09:45:50 -0400

    Apply pre-commit

--- tests/test_deploy_ioc_vars.py
+++ tests/test_deploy_ioc_vars.py
@@ -1,5 +1,4 @@
 import pytest
-from pathlib import Path
 import os
 
 
@@ -14,23 +13,36 @@ OPTIONAL_KEYS: dict[str, type] = {
     "deploy_ioc_manage_iocs_nextport": int,
     "deploy_ioc_as_dir_name": str,
     "deploy_ioc_device_specific_env": dict,
-    "deploy_ioc_load_as_substitutions": bool,
     "deploy_ioc_required_module": str,
 }
 
-DEPLOY_IOC_VARS_FILES = [os.path.splitext(f)[0] for f in os.listdir("roles/deploy_ioc/vars") if f.endswith(".yml")]
+DEPLOY_IOC_VARS_FILES = [
+    os.path.splitext(f)[0]
+    for f in os.listdir("roles/deploy_ioc/vars")
+    if f.endswith(".yml")
+]
 
-pytestmark = pytest.mark.parametrize("deploy_ioc_var_file", DEPLOY_IOC_VARS_FILES, indirect=True)
+pytestmark = pytest.mark.parametrize(
+    "deploy_ioc_var_file", DEPLOY_IOC_VARS_FILES, indirect=True
+)
+
 
 def test_deploy_ioc_var_file_has_matching_role(deploy_ioc_var_file):
     assert os.path.exists(os.path.join("roles", deploy_ioc_var_file.name))
+
 
 def test_deploy_ioc_var_files_valid(deploy_ioc_var_file):
     for key in deploy_ioc_var_file.data:
         assert key in OPTIONAL_KEYS
         assert type(deploy_ioc_var_file.data[key]) is OPTIONAL_KEYS[key]
 
+
 def test_deploy_ioc_var_file_required_module_exists(deploy_ioc_var_file):
     if "deploy_ioc_required_module" in deploy_ioc_var_file.data:
         if deploy_ioc_var_file.data["deploy_ioc_required_module"]:
-            assert os.path.exists(os.path.join("roles/install_module/vars", f"{deploy_ioc_var_file.data["deploy_ioc_required_module"]}.yml"))
+            assert os.path.exists(
+                os.path.join(
+                    "roles/install_module/vars",
+                    f"{deploy_ioc_var_file.data['deploy_ioc_required_module']}.yml",
+                )
+            )
--- tests/test_install_module_vars.py
+++ tests/test_install_module_vars.py
@@ -1,9 +1,6 @@
 import os
-from typing import Callable
 
-from click import Path
 import pytest
-import yaml
 
 
 REQUIRED_KEYS: dict[str, type] = {
@@ -21,41 +18,66 @@ OPTIONAL_KEYS: dict[str, type] = {
 }
 
 
-INSTALL_MODULE_FILES = [os.path.splitext(f)[0] for f in os.listdir("roles/install_module/vars") if f.endswith(".yml")]
+INSTALL_MODULE_FILES = [
+    os.path.splitext(f)[0]
+    for f in os.listdir("roles/install_module/vars")
+    if f.endswith(".yml")
+]
 
 
-pytestmark = pytest.mark.parametrize("install_module_var_file", INSTALL_MODULE_FILES, indirect=True)
+pytestmark = pytest.mark.parametrize(
+    "install_module_var_file", INSTALL_MODULE_FILES, indirect=True
+)
+
 
 def test_install_module_vars_files_valid(install_module_var_file):
-
     assert len(list(install_module_var_file.data.keys())) == 1
     assert list(install_module_var_file.data.keys())[0] == install_module_var_file.name
-
 
     # Make sure all required keys are present
     for key in REQUIRED_KEYS:
         assert key in install_module_var_file.data[install_module_var_file.name]
-        assert isinstance(install_module_var_file.data[install_module_var_file.name][key], REQUIRED_KEYS[key])
+        assert isinstance(
+            install_module_var_file.data[install_module_var_file.name][key],
+            REQUIRED_KEYS[key],
+        )
 
     # Check that optional keys, if present, are of correct type
     for key in OPTIONAL_KEYS:
         if key in install_module_var_file.data[install_module_var_file.name]:
-            assert isinstance(install_module_var_file.data[install_module_var_file.name][key], OPTIONAL_KEYS[key])
+            assert isinstance(
+                install_module_var_file.data[install_module_var_file.name][key],
+                OPTIONAL_KEYS[key],
+            )
 
     # Ensure no additional keys present
     for key in install_module_var_file.data[install_module_var_file.name]:
         assert key in REQUIRED_KEYS or key in OPTIONAL_KEYS
 
-def test_install_module_vars_files_all_module_deps_exist(install_module_var_file, ):
 
+def test_install_module_vars_files_all_module_deps_exist(
+    install_module_var_file,
+):
     if "module_deps" in install_module_var_file.data[install_module_var_file.name]:
-        for module_dep in install_module_var_file.data[install_module_var_file.name]["module_deps"]:
+        for module_dep in install_module_var_file.data[install_module_var_file.name][
+            "module_deps"
+        ]:
             assert module_dep in INSTALL_MODULE_FILES
+
 
 def test_ensure_version_suffixed(install_module_var_file):
     assert install_module_var_file.name.endswith(
-        install_module_var_file.data[install_module_var_file.name]["version"].replace("-", "_")
-    ) or install_module_var_file.name.endswith(install_module_var_file.data[install_module_var_file.name]["version"].replace(".", "_"))
+        install_module_var_file.data[install_module_var_file.name]["version"].replace(
+            "-", "_"
+        )
+    ) or install_module_var_file.name.endswith(
+        install_module_var_file.data[install_module_var_file.name]["version"].replace(
+            ".", "_"
+        )
+    )
+
 
 def test_ensure_no_main_or_master_branch(install_module_var_file):
-    assert install_module_var_file.data[install_module_var_file.name]["version"] not in ["main", "master"]
+    assert install_module_var_file.data[install_module_var_file.name][
+        "version"
+    ] not in ["main", "master"]
