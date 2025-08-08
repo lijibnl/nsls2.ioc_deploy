f68b6852e32a776e3472f190451a0c531be7b09d
Jakub Wlodek <jwlodek@bnl.gov>
2025-08-08 12:10:23 -0400

    Use yamale for base schema

--- tests/test_validate_ex_configs_against_schemas.py
+++ tests/test_validate_ex_configs_against_schemas.py
@@ -3,7 +3,6 @@ import os
 import pytest
 import yamale
 import yaml
-from schema import Optional, Or, Schema
 
 COMMON_ROLES = [
     "install_module",
@@ -15,60 +14,46 @@ COMMON_ROLES = [
 
 DEVICE_ROLES = [role for role in os.listdir("roles") if role not in COMMON_ROLES]
 
-BASE_IOC_CONFIG_SCHEMA = Schema(
-    {
-        "type": Or(*DEVICE_ROLES),
-        "environment": {str: Or(str, int, float)},
-        Optional("substitutions"): {
-            str: Or(
-                {
-                    "templates": [
-                        {
-                            "filepath": str,
-                            "pattern": [str],
-                            "instances": [[str, int, float]],
-                        }
-                    ],
-                    Optional("template_macros"): str,
-                    Optional("load_as_template"): bool,
-                },
-                [{"filepath": str, "pattern": [str], "instances": [[str, int, float]]}],
-            )
-        },
-        Optional("required_module"): str,
-        Optional("executable"): str,
-    },
-    ignore_extra_keys=True,
-)
+
+class IOCTypeValidator(yamale.validators.Validator):
+    tag = "ioc_type"
+
+    def _is_valid(self, value):
+        return value in DEVICE_ROLES
 
 
 pytestmark = pytest.mark.parametrize("device_role", DEVICE_ROLES)
 
 
 def test_ensure_example_present(device_role):
-    assert os.path.exists(
-        os.path.join("roles", device_role, "example.yml")
-    ), f"Example configuration file for {device_role} role is missing."
+    assert os.path.exists(os.path.join("roles", device_role, "example.yml")), (
+        f"Example configuration file for {device_role} role is missing."
+    )
 
 
 def test_ensure_required_schema_present(device_role):
     schema_path = os.path.join("roles", device_role, "schema.yml")
-    assert os.path.exists(
-        schema_path
-    ), f"Schema file for {device_role} role is missing at {schema_path}."
+    assert os.path.exists(schema_path), f"Schema file for {device_role} role is missing at {schema_path}."
 
 
 def test_ensure_example_validates_with_base_schema(device_role):
-    with open(os.path.join("roles", device_role, "example.yml"), "r") as fp:
+    with open(os.path.join("roles", device_role, "example.yml")) as fp:
         example_data = yaml.safe_load(fp)
         ioc_name = list(example_data.keys())[0]
         ioc_config = example_data[ioc_name]
-        try:
-            BASE_IOC_CONFIG_SCHEMA.validate(ioc_config)
-        except Exception as e:
-            pytest.fail(
-                f"Ex. config for {ioc_name} in {device_role} role doesn't conform to the base IOC config schema: {e}"
-            )
+
+    validators = yamale.validators.DefaultValidators.copy()
+    validators["ioc_type"] = IOCTypeValidator
+
+    data = yamale.make_data(content=yaml.dump(ioc_config))
+    base_schema = yamale.make_schema("roles/deploy_ioc/schema.yml", validators=validators)
+
+    try:
+        yamale.validate(base_schema, data, strict=False)
+    except Exception as e:
+        pytest.fail(
+            f"Ex. config for {ioc_name} in {device_role} role doesn't conform to the base IOC config schema: {e}"
+        )
 
 
 def test_ensure_example_validates_with_role_specific_schema(device_role):
@@ -77,7 +62,7 @@ def test_ensure_example_validates_with_role_specific_schema(device_role):
 
     schema = yamale.make_schema(schema_path)
 
-    with open(example_path, "r") as fp:
+    with open(example_path) as fp:
         example_data = yaml.safe_load(fp)
         ioc_name = list(example_data.keys())[0]
         ioc_config = example_data[ioc_name]
@@ -87,6 +72,4 @@ def test_ensure_example_validates_with_role_specific_schema(device_role):
     try:
         yamale.validate(schema, data, strict=False)
     except yamale.YamaleError as e:
-        pytest.fail(
-            f"Ex. config for {ioc_name} in {device_role} role doesn't conform to the role-specific schema: {e}"
-        )
+        pytest.fail(f"Ex. config for {ioc_name} in {device_role} role doesn't conform to the role-specific schema: {e}")
