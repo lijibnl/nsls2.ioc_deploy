23264de8c3971831ccdd1aba00c71ea6124490f7
Jakub Wlodek <jwlodek@bnl.gov>
2025-08-25 13:31:05 -0400

    Move validation of var files to yamale for consistency

--- tests/test_deploy_ioc_vars.py
+++ tests/test_deploy_ioc_vars.py
@@ -10,9 +10,22 @@ DEPLOY_IOC_VARS_FILES = [
     if f.endswith(".yml")
 ]
 
+INSTALL_MODULE_FILES = [
+    os.path.splitext(f)[0]
+    for f in os.listdir("roles/install_module/vars")
+    if f.endswith(".yml")
+]
+
+
 pytestmark = pytest.mark.parametrize(
     "deploy_ioc_var_file", DEPLOY_IOC_VARS_FILES, indirect=True
 )
+
+class ModuleNameValidator(yamale.validators.Validator):
+    tag = "module_name"
+
+    def _is_valid(self, value: str) -> bool:
+        return value in INSTALL_MODULE_FILES
 
 
 def test_deploy_ioc_var_file_has_matching_role(deploy_ioc_var_file):
@@ -22,7 +35,9 @@ def test_deploy_ioc_var_file_has_matching_role(deploy_ioc_var_file):
 def test_deploy_ioc_var_files_valid(deploy_ioc_var_file):
     if deploy_ioc_var_file.data:
         data = yamale.make_data(content=yaml.dump(deploy_ioc_var_file.data))
-        schema = yamale.make_schema("schemas/device_specific_vars.yml")
+        validators = yamale.validators.DefaultValidators.copy()
+        validators["module_name"] = ModuleNameValidator
+        schema = yamale.make_schema("schemas/device_specific_vars.yml", validators=validators)
         try:
             yamale.validate(schema, data)
         except Exception as e:
